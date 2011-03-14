8a3518690fa2621b012bfd4cdc40308e53c19438
StevenMaschmeyer@gmail.com <StevenMaschmeyer@gmail.com@fbdc7275-83b5-1b1c-cf40-f337e8043d1d>
2011-03-14 21:23:35 +0000

    Added CPATH to makefile; made login work for me.

--- Login.jsp
+++ Login.jsp
@@ -8,7 +8,7 @@
 			</TR>
 			<TR VALIGN=TOP ALIGN=LEFT>
 			<TD><B><I>Password:</I></B></TD>
-			<TD><INPUT TYPE="password" NAME="PASSWD"></TD>
+			<TD><INPUT TYPE="password" NAME="password"></TD>
 			</TR>
 			</TABLE>
 			<INPUT TYPE="submit" NAME="Submit" VALUE="LOGIN">
---
+++ doc/testdata.sql
@@ -0,0 +1,72 @@
+-- Test Data 1
+
+@setup.sql;
+
+
+-- users(user_name,password,class,date_registered)
+insert into users values ('a1', 'a1', 'a', '1/jan/2011');
+insert into users values ('a2', 'a2', 'a', '1/jan/2011');
+insert into users values ('r1', 'r1', 'r', '10/jan/2011');
+insert into users values ('r2', 'r2', 'r', '11/jan/2011');
+insert into users values ('d1', 'd1', 'd', '1/feb/2011');
+insert into users values ('d2', 'd2', 'd', '2/feb/2011');
+insert into users values ('d3', 'd3', 'd', '3/feb/2011');
+insert into users values ('p1', 'p1', 'p', '1/mar/2011');
+insert into users values ('p2', 'p2', 'p', '1/mar/2011');
+insert into users values ('p3', 'p3', 'p', '1/mar/2011');
+insert into users values ('Steven', 'Steven', 'p', '2/mar/2011');
+insert into users values ('Oscar', 'Oscar', 'p', '2/mar/2011');
+insert into users values ('Camden', 'Camden', 'p', '3/mar/2011');
+
+-- persons(user_name,first_name,last_name,address,email,phone)
+insert into persons values ('a1', 'Admin', 'One', '1 Admin Road', 'a1@radiology.com', '7801111111');
+insert into persons values ('a2', 'Admin', 'Two', '2 Admin Road', 'a2@radiology.com', '7801112222');
+insert into persons values ('r1', 'Radio', 'One', '1 Radio Ave', 'r1@radiology.com', '7802221111');
+insert into persons values ('r2', 'Radio', 'Two', '2 Radio Ave', 'r2@radiology.com', '7802222222');
+insert into persons values ('d1', 'Doctor', 'One', '1 Doctor Street', 'd1@radiology.com', '7803331111');
+insert into persons values ('d2', 'Doctor', 'Two', '2 Doctor Street', 'd2@radiology.com', '7803332222');
+insert into persons values ('d3', 'Doctor', 'Three', '3 Doctor Street', 'd3@radiology.com', '7803333333');
+insert into persons values ('p1', 'Patient', 'One', '1 Patient Rd', 'p1@radiology.com', '7804441111');
+insert into persons values ('p2', 'Patient', 'Two', '2 Patient Rd', 'p2@radiology.com', '7804442222');
+insert into persons values ('p3', 'Patient', 'Three', '3 Patient Rd', 'p3@radiology.com', '7804443333');
+insert into persons values ('Steven', 'Steven', 'Maschmeyer', '4255 42 AVE', 'maschmey@ualberta.ca', '7809869063');
+insert into persons values ('Oscar', 'Oscar', 'Ramirez', '2 Db Street', 'oramirez@ualberta.ca', '7804445555');
+insert into persons values ('Camden', 'Camden', 'Narzt', '3 Db Street', 'narzt@ualberta.ca', '7804446666');
+
+-- family_doctor(doctor_name,patient_name
+insert into family_doctor values ('d1', 'p1');
+insert into family_doctor values ('d1', 'Steven');
+insert into family_doctor values ('d1', 'Oscar');
+insert into family_doctor values ('d2', 'p2');
+insert into family_doctor values ('d2', 'Camden');
+insert into family_doctor values ('d3', 'p3');
+
+-- radiology_record(record_id,patient_name,doctor_name,radiologist_name,test_type,prescribing_date,test_date,diagnosis, description)
+insert into radiology_record values (1, 'p1', 'd1', 'r1', 'MRI', '4/mar/2011', '4/mar/2011', 'no cancer', 'Screening for brain cancer.');
+insert into radiology_record values (2, 'p1', 'd1', 'r1', 'CAT', '4/mar/2011', '4/mar/2011', 'cancer', 'Screening for lung cancer.');
+insert into radiology_record values (3, 'p1', 'd1', 'r1', 'US', '4/mar/2011', '4/mar/2011', 'heart ok', 'Screening for heart problems.');
+insert into radiology_record values (4, 'p1', 'd1', 'r1', 'MRI', '4/mar/2011', '4/mar/2011', 'sinitus', 'Screening for sinitus.');
+insert into radiology_record values (5, 'p1', 'd2', 'r2', 'CAT', '5/mar/2011', '5/mar/2011', 'kidney stones', 'Screening for kidney stones.');
+insert into radiology_record values (6, 'p1', 'd2', 'r2', 'CAT', '5/mar/2011', '5/mar/2011', 'fractured rib', 'Complaining of chest pain - has fractured rib.');
+insert into radiology_record values (7, 'p3', 'd3', 'r1', 'MRI', '7/mar/2011', '7/mar/2011', 'no sinitus', 'Screening for sinitus.');
+insert into radiology_record values (8, 'p3', 'd3', 'r1', 'MRI', '4/mar/2011', '4/mar/2011', 'no abnormalities', 'Brain is free from abnormalities.');
+insert into radiology_record values (9, 'Steven', 'd2', 'r1', 'MRI', '5/mar/2011', '5/mar/2011', 'arachnoid cyst', 'Determining cause of head pain.');
+insert into radiology_record values (10, 'Steven', 'd2', 'r2', 'CAT', '6/mar/2011', '6/mar/2011', 'fractured cranium', 'Determine cause of head inflamation.');
+insert into radiology_record values (11, 'Oscar', 'd1', 'r1', 'US', '2/mar/2011', '2/mar/2011', 'heart ok', 'Screening for heart problems.');
+insert into radiology_record values (12, 'Camden', 'd2', 'r2', 'CAT', '3/mar/2011', '3/mar/2011', 'sinitus', 'Screening for sinitus.');
+
+-- pacs_images(record_id,image_id,thumbnail,regular_size,full_size)
+insert into pacs_images values (1, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (1, 2, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (1, 3, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (2, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (3, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (4, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (5, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (6, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (7, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (8, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (9, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (10, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (11, 1, empty_blob(), empty_blob(), empty_blob());
+insert into pacs_images values (12, 1, empty_blob(), empty_blob(), empty_blob());
