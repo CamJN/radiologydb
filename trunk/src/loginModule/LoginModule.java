bd2579dc217299f67d8753df721ae9a31e7eaae6
oars89@gmail.com <oars89@gmail.com@fbdc7275-83b5-1b1c-cf40-f337e8043d1d>
2011-03-17 15:42:48 +0000

    Fixed session, added session variable UserClass

--- trunk/src/loginModule/LoginModule.java
+++ trunk/src/loginModule/LoginModule.java
@@ -1,35 +1,34 @@
 package loginModule;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import java.sql.*;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpSession;
 
 import util.ConnectionManager;
 
 @SuppressWarnings("serial")
-public class LoginModule extends GenericServlet {
-    public void service(ServletRequest request, ServletResponse response)
-    throws IOException
-    {
-    	String username = request.getParameter("username");
+public class LoginModule extends HttpServlet {
+  public void doGet( HttpServletRequest request, HttpServletResponse response )
+	throws ServletException, IOException {
+
+		response.setContentType("text/html");
+    		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 
 		PrintWriter out = response.getWriter();
 		
-		//HttpSession session = request.getSession(true);
-		//session.setAttribute("username", username);
-		//session.setAttribute("password", password);
-		
+				
 		ConnectionManager conManager = new ConnectionManager();
 
 		Statement stmt;
 		ResultSet rset = null;
 
-		String userSearch = "select password from users where user_name = '"
+		String userSearch = "select password, class from users where user_name = '"
 				+ username + "'";
 		
 		out.println("Username: " + username);
@@ -40,13 +39,27 @@ public class LoginModule extends GenericServlet {
 			rset = conManager.exec(userSearch);
 
 			String truepwd = "";
+			String userClass = "";
 
 			while (rset != null && rset.next())
+			{
 				truepwd = (rset.getString(1)).trim();
+				userClass = rset.getString(2).trim();
+
+			}
 
 			// display the result
 			if (password.equals(truepwd))
+			{
 				out.println("<p><b>Your Login is Successful!</b></p>");
+				out.println("<p>" + userClass + "</p>");
+				HttpSession session = request.getSession(true);
+				session.setAttribute("username", username);
+				session.setAttribute("password", password);
+				session.setAttribute("class", userClass);
+
+				out.println("<A HREF=NextPage.jsp> Next <A/>");
+			}
 			else
 				out.println("<p><b>Either your userName or Your password is inValid!</b></p>");
 
@@ -55,6 +68,26 @@ public class LoginModule extends GenericServlet {
 			System.err.println("SQLException: " + ex.getMessage());
 
 		}
+		
+		
+    }
+
+
+    /**
+     * Handles HTTP POST request
+     */
+    public void doPost( HttpServletRequest request,
+			HttpServletResponse response )
+	throws ServletException, IOException {
+	// Invoke doGet to process this request
+	doGet( request, response );
+    }
+   
+    /**
+     * Returns a brief description about this servlet
+     */
+    public String getServletInfo() {
+	return "Servlet that returns Session information";
     }
 }
                                                                             
