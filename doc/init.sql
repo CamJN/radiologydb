5ca7f19885b7437a0e29784a0bdec0419d85231e
StevenMaschmeyer@gmail.com <StevenMaschmeyer@gmail.com@fbdc7275-83b5-1b1c-cf40-f337e8043d1d>
2011-04-01 20:05:20 +0000

    extracted our setup into init.sql

--- doc/init.sql
+++ doc/init.sql
@@ -1,14 +1,18 @@
+DROP SEQUENCE pic_id_sequence;
+
 DROP INDEX radioname_index;
 DROP INDEX radiodiag_index;
 DROP INDEX radiodesc_index;
 
-CREATE INDEX radioname_index ON radiology_record(patient_name) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
-CREATE INDEX radiodiag_index ON radiology_record(diagnosis) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
-CREATE INDEX radiodesc_index ON radiology_record(description) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
-
 DROP INDEX uname_index;
 DROP INDEX fname_index;
 DROP INDEX lname_index;
+
+CREATE SEQUENCE pic_id_sequence;
+
+CREATE INDEX radioname_index ON radiology_record(patient_name) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
+CREATE INDEX radiodiag_index ON radiology_record(diagnosis) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
+CREATE INDEX radiodesc_index ON radiology_record(description) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
 
 CREATE INDEX uname_index ON persons(user_name) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
 CREATE INDEX fname_index ON persons(first_name) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
