a4cb771cac9d6b13d030882bf353f039da64327f
StevenMaschmeyer@gmail.com <StevenMaschmeyer@gmail.com@fbdc7275-83b5-1b1c-cf40-f337e8043d1d>
2011-04-04 22:51:56 +0000

    init fix

--- trunk/doc/init.sql
+++ trunk/doc/init.sql
@@ -1,4 +1,5 @@
 DROP SEQUENCE pic_id_sequence;
+DROP SEQUENCE rec_id_sequence;
 
 DROP INDEX radioname_index;
 DROP INDEX radiodiag_index;
@@ -8,8 +9,13 @@ DROP INDEX uname_index;
 DROP INDEX fname_index;
 DROP INDEX lname_index;
 
-CREATE SEQUENCE pic_id_sequence;
-CREATE SEQUENCE rec_id_sequence;
+COL maxid NEW_VALUE _maxid
+SELECT NVL(MAX(record_id),0)+1 maxid from radiology_record;
+CREATE SEQUENCE rec_id_sequence NOMAXVALUE START WITH &&_maxid INCREMENT BY 1;
+
+COL maxid NEW_VALUE _maxid
+SELECT NVL(MAX(image_id),0)+1 maxid FROM pacs_images;
+CREATE SEQUENCE pic_id_sequence NOMAXVALUE START WITH &&_maxid INCREMENT BY 1;
 
 CREATE INDEX radioname_index ON radiology_record(patient_name) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
 CREATE INDEX radiodiag_index ON radiology_record(diagnosis) INDEXTYPE IS CTXSYS.CONTEXT PARAMETERS ('SYNC ( ON COMMIT)');
